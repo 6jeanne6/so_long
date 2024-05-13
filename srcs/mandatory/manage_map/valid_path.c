945bcea58ccb96058806e339fb8347eae93d82a8
Jeanne Wu <jewu@made-f0Br3s8.clusters.42paris.fr>
2024-05-13 15:32:09 +0200

    check loaded image

--- srcs/mandatory/manage_map/valid_path.c
+++ srcs/mandatory/manage_map/valid_path.c
@@ -6,7 +6,7 @@
 /*   By: jewu <jewu@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/24 15:47:55 by jewu              #+#    #+#             */
-/*   Updated: 2024/04/25 15:27:57 by jewu             ###   ########.fr       */
+/*   Updated: 2024/05/12 16:22:20 by jewu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ static int	create_map_tmp(t_mlx *so_long)
 	int	i;
 
 	i = -1;
-	so_long->map_tmp = ft_calloc(so_long->column, sizeof(char *));
+	so_long->map_tmp = ft_calloc(so_long->row, sizeof(char *));
 	if (!so_long->map_tmp)
 		destroy_and_tab(so_long);
 	while (++i < so_long->row)
@@ -89,7 +89,7 @@ int	search_path(t_mlx *so_long)
 		if (fill_exit(so_long, so_long->e->pos))
 			return (0);
 	}
-	free_tab_str(so_long->map_tmp);
+	free_tab_str_tmp(so_long->map_tmp, so_long);
 	return (-1);
 }
 //process of searching a valid path
